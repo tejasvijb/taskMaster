a890ddbc37ab5891c6537c2e763b8d6983982a79
tejasvijb <jbtejasvi@gmail.com>
2026-02-23 18:19:11 +0530

    Refactor database connection to use Pool instead of Client and utilize environment variables

--- backend/src/api/v1/config/dbConnection.ts
+++ backend/src/api/v1/config/dbConnection.ts
@@ -1,8 +1,8 @@
-import { Client } from "pg"
+import { Pool } from "pg"
 
 
-const pgClient = new Client({
-  database: "taskmaster",
+export const pgPool = new Pool({
+  database: process.env.PG_DATABASE,
   host: process.env.PG_HOST,
   password: process.env.PG_PASSWORD,
   port: 5432,
@@ -11,8 +11,8 @@ const pgClient = new Client({
 
 const connectDb = async () => {
   try {
-    await pgClient.connect();
-    console.log(`PostgreSQL Connected: ${pgClient.host}, ${pgClient.database}`);
+    await pgPool.connect();
+    console.log(`PostgreSQL Connected: ${pgPool.options.host}, ${pgPool.options.database}`);
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
