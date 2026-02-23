0fb9f12197e3eb732ee3c688d09523c58e808168
tejasvijb <jbtejasvi@gmail.com>
2026-02-23 18:05:55 +0530

    Replace MongoDB with PostgreSQL for database connection and update dependencies

--- backend/src/api/v1/config/dbConnection.ts
+++ backend/src/api/v1/config/dbConnection.ts
@@ -1,9 +1,18 @@
-import mongoose from "mongoose";
+import { Client } from "pg"
+
+
+const pgClient = new Client({
+  database: "taskmaster",
+  host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+  user: process.env.PG_USER,
+});
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI || "");
-    console.log(`MongoDB Connected: ${connect.connection.host}, ${connect.connection.name}`);
+    await pgClient.connect();
+    console.log(`PostgreSQL Connected: ${pgClient.host}, ${pgClient.database}`);
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
